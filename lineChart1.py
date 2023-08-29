afc315b1e02c5f9f2d11f9bbb98a088e2f101822
ankitaData <ankitasaharay22@gmail.com>
2023-08-29 19:11:35 +0530

    updated lineChart1

--- lineChart1.py
+++ lineChart1.py
@@ -9,14 +9,14 @@ np.random.seed(56)
 x_values = np.linspace(0, 1, 100)
 y_values = np.random.randn(100)
 
-trace0 = go.Scatter(x=x_values, y=y_values+5,
-                   mode='markers', name='markers')
+trace0 = go.Scatter(x=x_values, y=y_values + 5,
+                    mode='markers', name='markers')
 
 trace1 = go.Scatter(x=x_values, y=y_values,
-                   mode='lines', name='mylines')
+                    mode='lines', name='mylines')
 
-trace2 = go.Scatter(x=x_values, y=y_values-5,
-                   mode='lines+markers', name='lines and markers')
+trace2 = go.Scatter(x=x_values, y=y_values - 5,
+                    mode='lines+markers', name='lines and markers')
 data = [trace0, trace1, trace2]
 
 layout = go.Layout(title='Line Charts')
