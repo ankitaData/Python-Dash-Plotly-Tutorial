a3e917f1398a8043f9455de74d07c80910943c40
ankitaData <ankitasaharay22@gmail.com>
2023-08-29 15:06:39 +0530

    updated Bubble Charts

--- bubbleCharts.py
+++ bubbleCharts.py
@@ -4,6 +4,8 @@ import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
+# creating a dataframe
+
 file_path = 'data/mpg.csv'
 df = pd.read_csv(file_path)
 pd.set_option('display.max_columns', 9)
